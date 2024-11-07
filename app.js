1cc46ff47b7f5df1556005981902723c2b92eaa0
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-11-06 17:33:22 -0800

    JavaScript functionality

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const balance = document.getElementById("balance");
 const expense = document.getElementById("expense");
+const addBtn = document.getElementById("add-btn");
 const expensesList = document.getElementById("expenses-list");
+
+class Account {
+    constructor() {
+        this.balance = 1323.50;
+        balance.textContent = this.balance;
+    }
+
+    addExpense() {
+        let loss = parseFloat(expense.value);
+        this.balance -= loss;
+
+        let expenseItem = document.createElement("li");
+        expenseItem.textContent = "-$" + loss;
+        expensesList.appendChild(expenseItem);
+
+        balance.textContent = this.balance;
+        expense.value = "";
+    }
+}
+
+const account = new Account();
+addBtn.addEventListener('click', account.addExpense.bind(account));
 
 
