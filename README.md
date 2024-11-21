dd3e571723bf94a435d38d2d11563defdea21502
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-11-20 17:02:23 -0800

    Refactoring for state data selection

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Betwise is a web application for managing your gambling 
 budget.
 
-With this application, you can:
+## Features:
 
 - Create a custom budget
 
@@ -13,25 +13,7 @@ With this application, you can:
 
 - Access Nevada resources for responsible spending habits
 
----
+## Devlog
 
-## Development 
-
-### Completed
-
-1. Create a new Github repository called "BetWise" in your 
-account.
-
-2. Create a new index.html file
-
-3. Add boilerplate code using the `html:5` command
-
-4. Start a local live server 
-
-5. Send link in chat with repository and boilerplate HTML.
-
-6. Add header with navigation links
-
-### In Progress
-
-1. Create budget input element
+(11/20/24 - 16:37) We refactored the states data selection method by creating a 
+JSON file to improve searchability. This is to help practice AJAX requests.
--- app.js
+++ app.js
@@ -3,47 +3,63 @@ const amountInput = document.getElementById("amount");
 const amountType = document.getElementById("type");
 const addBtn = document.getElementById("add-btn");
 const expensesList = document.getElementById("expenses-list");
-const states = document.getElementById("states");
+const statesDropdown = document.getElementById("states");
 
-function generateDrop() {
-  for (let i = 0; i < statesIncome.length; i++) {
+function generateDropdownItems() {
+  for (let i = 0; i < statesIncomeData.length; i++) {
     let opt = document.createElement("option");
-    opt.value = statesIncome[i].state;
-    opt.textContent = statesIncome[i].state;
+    opt.value = statesIncomeData[i].state;
+    opt.textContent = statesIncomeData[i].state;
     states.appendChild(opt);
   }
 }
 
-generateDrop();
+generateDropdownItems();
 
 class Account {
-    constructor() {
-        this.balance = 2500.00.toFixed(2);
-        balanceLabel.textContent = this.balance;
+  constructor() {
+    this.balance = 2500.00.toFixed(2);
+    balanceLabel.textContent = this.balance;
+  }
+
+  updateBalance() {
+    let amount = parseFloat(amountInput.value);
+    let type = amountType.value;
+    let item = document.createElement("li");
+    let prefix = "";
+
+    if (type == "loss") {
+      this.balance -= amount;
+      item.style.color = "red";
+      prefix = "-$";
+    } else {
+      this.balance += amount;
+      item.style.color = "green";
+      prefix = "+$";
     }
-		
-		updateBalance() {
-			let amount = parseFloat(amountInput.value);
-			let type = amountType.value;
-			let item = document.createElement("li");
-			let prefix = "";
-			
-			if (type == "loss") {
-				this.balance -= amount;
-				item.style.color = "red";
-				prefix = "-$";
-			} else {
-				this.balance += amount;
-				item.style.color = "green";
-				prefix = "+$";
-			}
-			
-			item.textContent = prefix + amount.toFixed(2);
-			expensesList.appendChild(item);
-			balanceLabel.textContent = this.balance.toFixed(2);
-			amountInput.value = "";
-		}
+
+    item.textContent = prefix + amount.toFixed(2);
+    expensesList.appendChild(item);
+    balanceLabel.textContent = this.balance.toFixed(2);
+    amountInput.value = "";
+  }
+  
+  updateLocation(state) {
+    this.balance = state.average.toFixed(2);
+    balanceLabel.textContent = this.balance;
+  }
 }
 
 const account = new Account();
 addBtn.addEventListener("click", account.updateBalance.bind(account));
+
+statesDropdown.addEventListener("change", () => { 
+  fetch("./states.json") 
+    .then(response => response.json()) 
+    .then(data => {
+      const selectedState = statesDropdown.value;
+      const stateData = data.find((dataObj) => dataObj.state === selectedState);
+      account.updateLocation(stateData);
+    })
+    .catch(error => console.error("Error fetching JSON:", error))
+})
